7241151ab8cae4379c32385952ff51cd8dadb99e
saif1642 <islumsaiful@gmail.com>
2021-09-02 20:53:57 +0600

    authentication setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { LoginDTO, RegisterDTO } from 'src/models/user.dto';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+  ) {}
+
+  async register(credentials: RegisterDTO) {
+    try {
+      const user = this.userRepo.create(credentials);
+      await user.save();
+      return user;
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username has already been taken');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async login({ email, password }: LoginDTO) {
+    try {
+      const user = await this.userRepo.findOne({ where: { email } });
+      const isValid = await user.comparePassword(password);
+      if (!isValid) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      return user;
+    } catch (err) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+}
